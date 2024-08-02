221d407c45864c283706f4161290e5cbba25fed9
윤형찬 <72960152+velmash@users.noreply.github.com>
2024-08-02 11:27:52 +0900

    :wrench: 타입추론

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -37,7 +37,7 @@ struct CardView: View {
     
     var body: some View {
         ZStack {
-            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            let base = RoundedRectangle(cornerRadius: 12)
             
             if isFaceUp {
                 base.fill(.white)
