4ce18114847ad58b1d05483395ddbf2733dffbac
윤형찬 <72960152+velmash@users.noreply.github.com>
2024-08-02 11:25:29 +0900

    :wrench: Base View Builder

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -37,16 +37,16 @@ struct CardView: View {
     
     var body: some View {
         ZStack {
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
     //                .strokeBorder(style: StrokeStyle(lineWidth: 10, dash: [10, 2]))
                 Text("👻")
                     .font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
         }
     }
